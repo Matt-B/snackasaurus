9a74dea223a4d9d8e469550fd35624256e95ef4c
Matt Bailey <matt@thebaileyresidence.co.uk>
2012-05-20 12:40:16 +0100

    Created tag for recipe display and basic front page.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,7 +11,8 @@ public class Application extends Controller {
 
     public static void index() {
         Recipe frontRecipe = Recipe.find("order by posted desc").first();
-        render(frontRecipe);
+        List<Recipe> olderRecipes = Recipe.find("order by posted desc").from(1).fetch(4);
+        render(frontRecipe, olderRecipes);
     }
 
 }
