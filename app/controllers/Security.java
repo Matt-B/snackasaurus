8b13e1b636fe237e4cbb46a9bd15f186b2edb1bf
Matt B <matt@thebaileyresidence.co.uk>
2012-11-18 00:30:18 +0000

    Started styling login page by overriding secure module defaults.

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -8,8 +8,4 @@ public class Security extends Secure.Security {
         return User.connect(username, password) != null;
     }
 
-    static void onDisconnect() {
-        Application.index();
-    }
-
 }
